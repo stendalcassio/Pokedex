d1359e088a63e72cc90bb1f11c460f223895c218
Stendal <stendalcassio@hotmail.com>
2023-09-19 16:53:10 -0300

    feat: construção e separação da requisição da api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,29 @@
-console.log("SUCESSO!");
+const convertPokemonsTypesToLi = (pokemonTypes) => {
+  return pokemonTypes.map(
+    (typeSlot) => `<li class="type">${typeSlot.type.name}</li>`
+  );
+};
+
+const convertPokemonToLi = (pokemon) => {
+  return `
+  <li class="pokemon">
+  <span class="number">#${pokemon.order}</span>
+  <span class="name">${pokemon.name}</span>
+  <div class="detail">
+    <ol class="types">
+      ${convertPokemonsTypesToLi(pokemon.types).join("")}
+    </ol>
+    <img
+      src="${pokemon.sprites.other.dream_world.front_default}"
+      alt="${pokemon.name}"
+    />
+  </div>
+</li>
+  `;
+};
+
+const pokemonList = document.getElementById("pokemonList");
+
+pokeApi.getPokemons().then((pokemons = []) => {
+  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
+});
